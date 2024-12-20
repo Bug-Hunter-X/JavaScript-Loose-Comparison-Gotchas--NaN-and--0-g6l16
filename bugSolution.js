09db1b0a7950ae00db0c3b503be549e208e858f1
BugHunter-X <askzenai@gmail.com>
2024-12-20 03:38:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,18 @@
+function foo(a, b) {
+  if (Object.is(a, b)) {
+    return true;
+  }
+  return false;
+}
+
+console.log(foo(NaN, NaN)); // true
+console.log(foo(0, -0)); // false
+
+function fooStrict(a, b) {
+  if (a === b) {
+    return true;
+  }
+  return false;
+}
+console.log(fooStrict(NaN, NaN)); // false
+console.log(fooStrict(0, -0)); //true 
